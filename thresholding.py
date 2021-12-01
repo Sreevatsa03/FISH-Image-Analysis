b64c2e8633866b57446acc73062d216de1e0803b
Soumili Dey <dey.sou@northeastern.edu>
2021-12-01 01:14:45 -0500

    adding centroid method

--- thresholding.py
+++ thresholding.py
@@ -18,25 +18,8 @@ def plot_image(image, title = ''):
     plt.imshow(RGB_img, 'gray', vmin = 0, vmax = 255)
     plt.title(title)
     plt.axis('off')
-    plt.show()
+    plt.savefig(('thresholding_output'.strip() + str(title).strip() + '.png'.strip()))
 
-def save_image(image):
-    # TODO: figure out how to save image
-    # Image path
-    # image = cv2.imread('C:/Users\19787\.spyder-py3\DS 2500\thresholding\image1.png')
-    
-    # # Image directory
-    # directory = r'C:\Users\19787\Desktop'
-
-    # img = cv2.imread('D:/image-1.png')
-    #do some transformations on img
-    
-    #save matrix/array as image file
-    isWritten = cv2.imwrite('D:/image-2.png', image)
-    
-    if isWritten:
-    	print('Image is successfully saved as file.')
-        
 # watershed 
 #https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_watershed/py_watershed.html
 
@@ -52,6 +35,7 @@ def watershed(filename, output = "plot"):
     else:
         cv2.imshow("Watershed", masked)
         cv2.waitKey(0)
+        cv2.imwrite("watershed.png", masked)
 
 # gaussian blurring
 #https://www.pyimagesearch.com/2021/04/28/opencv-thresholding-cv2-threshold/
@@ -65,25 +49,29 @@ def gaussian_blur(filename, kernel_size):
     dst = cv2.GaussianBlur(src,(kernel_size, kernel_size),cv2.BORDER_DEFAULT)
      
     # display output image as pop up window
-    image = cv2.imshow("Gaussian Smoothing", dst)
+    cv2.imshow("Gaussian Smoothing", dst)
     cv2.waitKey(0) # waits until a key is pressed
     cv2.destroyAllWindows() # destroys the window showing image
-    #save_image(image)
+    cv2.imwrite("gaussian_blur.png", dst)
+    
 
 def binary_threshold(filename, threshold, output = "plot"):
     image = file_to_image(filename)
     gray = image_to_grayscale(image)
-    ret, thresh1 = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
-    masked = cv2.bitwise_and(image, image, mask=thresh1)
+    ret, thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+    masked = cv2.bitwise_and(image, image, mask=thresh)
     
     if output == "plot":
         plot_image(masked,'Binary Threshold')  
-        #save_image(masked, name = 'TEST')
         
     else:
         cv2.imshow("Binary Threshold", masked)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+        cv2.imwrite("binary_threshold.png", masked)
+        
+    return thresh   
+    
 # Reference: https://www.geeksforgeeks.org/erosion-dilation-images-using-opencv-python/   
 def erosion(filename, iterations, output = "plot"):
     img = cv2.imread(filename, 0)
@@ -104,12 +92,47 @@ def erosion(filename, iterations, output = "plot"):
         cv2.imshow('Input', img)
         cv2.imshow('Erosion', img_erosion)
         cv2.imshow('Dilation', img_dilation) 
+        cv2.imwrite("erosion.png", img_erosion)
         cv2.waitKey(0)
 
+# Reference: https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
+def get_centroids(image, threshold):
+    centroids = []
+    img = file_to_image(image)
+    mask = np.zeros(img.shape, dtype=np.uint8)
+    thresh = binary_threshold(image, threshold)
     
-# if __name__ == "__main__":
-#     watershed('MAX_C4-SOX2_(G)._PAX6_(R)._PAX7_(FR)_40x_Spinal_Cords_Uninjured_001.tif', "plot")
-    #gaussian_blur('image2.tif', 11)
+    contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    for c in contours:
+        
+        # calculate moments for each contour
+        M = cv2.moments(c)
+        
+        # calculate x,y coordinate of center
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+           
+        else:
+            cX, cY = int(c[0][0][0]), int(c[0][0][1])
+           
+        coord = (cX, cY)
+        centroids.append(coord)
+        
+        cv2.circle(mask, (cX, cY), 1, (255, 255, 255), -1)
+       
+    #display the image
+    cv2.imwrite('centroids.png', mask)
+    cv2.waitKey(0)
+    
+    return centroids
+
+if __name__ == "__main__":
+    get_centroids('C2 (Pax6) thresholded dots.tif', 0)
+    #get_centroids('MAX_C3-SOX2_(G)._PAX6_(R)._PAX7_(FR)_40x_Spinal_Cords_Uninjured_001.tif', 200)
+    #watershed('MAX_C4-SOX2_(G)._PAX6_(R)._PAX7_(FR)_40x_Spinal_Cords_Uninjured_001.tif', "plot")
+    #gaussian_blur('MAX_C4-SOX2_(G)._PAX6_(R)._PAX7_(FR)_40x_Spinal_Cords_Uninjured_001.tif', 11)
     #binary_threshold('MAX_C3-SOX2_(G)._PAX6_(R)._PAX7_(FR)_40x_Spinal_Cords_Uninjured_001.tif', 190)
     #erosion('MAX_C3-SOX2_(G)._PAX6_(R)._PAX7_(FR)_40x_Spinal_Cords_Uninjured_001.tif', 100)
     
